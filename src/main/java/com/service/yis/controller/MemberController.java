790727cdf5094103c4bf66406d4ad13f529d7016
조민규 <inch1209@naver.com>
2022-05-02 23:58:23 +0900

    PostConstruct edit

--- src/main/java/com/service/yis/controller/MemberController.java
+++ src/main/java/com/service/yis/controller/MemberController.java
@@ -34,6 +34,7 @@ public class MemberController {
         memberRepository.save(new Member("userA","user","1234"));
         memberRepository.save(new Member("userB","user2","2345"));
         memberRepository.save(new Member("userC","user3","34345"));
+        memberRepository.save(new Member("userD","user4","34235"));
 
     }
 }
