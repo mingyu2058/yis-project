78be28507f36c3855e6adaa2a0dcebc2c6eefc38
root <root@yis.4njrxoi4r0aenclloqrxlma32f.syx.internal.cloudapp.net>
2022-05-25 13:14:54 +0000

    datacenter

--- src/main/java/com/service/yis/config/DatabaseConfig.java
+++ src/main/java/com/service/yis/config/DatabaseConfig.java
@@ -14,7 +14,6 @@ public class DatabaseConfig {
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
 
         /*aws*/
-<<<<<<< HEAD
         //dataSourceBuilder.username("yis");
         //dataSourceBuilder.password("1234");
         //dataSourceBuilder.url("jdbc:mysql://localhost:3306/yis?useSSL=false&serverTimezone=UTC");
@@ -24,17 +23,8 @@ public class DatabaseConfig {
         dataSourceBuilder.username("yis");
         dataSourceBuilder.password("1234");
         dataSourceBuilder.url("jdbc:mysql://localhost:3306/yis?useSSL=false&serverTimezone=UTC");
-=======
-        //dataSourceBuilder.username("yis");
-        //dataSourceBuilder.password("");
-        //dataSourceBuilder.url("jdbc:mysql://:3306/yis?useSSL=false&serverTimezone=UTC");
+        
 
-
-        /* 로컬 */
-        //dataSourceBuilder.username("root");
-        //dataSourceBuilder.password("20582962");
-        //dataSourceBuilder.url("jdbc:mysql://localhost:3333/yis?useSSL=false&serverTimezone=UTC");
->>>>>>> b944f22d97dc4769aa90bcca65523d1954780060
         return dataSourceBuilder.build();
     }
 }
---
+++ src/main/java/com/service/yis/service/Data_Center_Service.java
@@ -0,0 +1,30 @@
+package com.service.yis.service;
+
+import com.service.yis.domain.item.Item_Entity;
+import com.service.yis.domain.item.Item_Repository;
+import com.service.yis.domain.item.Item_Entity;
+import com.service.yis.domain.item.Item_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class Data_Center_Service {
+    @Autowired
+    private Item_Repository item_Repository;
+
+    public List<Item_Entity> getItem(){return item_Repository.getItem();}
+    public List<Item_Entity> getItemAvgByDate(String name) {
+        return item_Repository.getItemAvgByDate(name);
+    }
+    public List<Item_Entity> getItemAvgByDateMarket(String name, String date) {return item_Repository.getItemAvgByDateMarket(name, date);}
+    public List<Item_Entity> getItemAllAvgByDate(String name) {return item_Repository.getItemAllAvgByDate(name);}
+    public List<Item_Entity> getItemByBtwDateAndDate(String name, String startDate, String endDate) {return item_Repository.getItemByBtwDateAndDate(name, startDate, endDate);}
+    public List<Item_Entity> getItemAvgMonth(String name) {return item_Repository.getItemAvgMonth(name);}
+    public List<Item_Entity> getItemAvgYear(String name) {return item_Repository.getItemAvgYear(name);}
+    public List<Item_Entity> getItemAvg3Month(String name) {return item_Repository.getItemAvg3Month(name);}
+    public List<Item_Entity> getItemAvg6Month(String name) {return item_Repository.getItemAvg6Month(name);}
+    public List<Item_Entity> getItemAvgByKind(String name) {return item_Repository.getItemAvgByKind(name);}
+
+}
