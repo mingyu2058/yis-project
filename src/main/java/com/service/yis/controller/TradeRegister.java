86292fd6354552632d53f65ae478323ae4555bea
root <root@yis.4njrxoi4r0aenclloqrxlma32f.syx.internal.cloudapp.net>
2022-06-10 17:04:13 +0000

    migration

--- src/main/java/com/service/yis/controller/TradeRegister.java
+++ src/main/java/com/service/yis/controller/TradeRegister.java
@@ -4,6 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/tradeRegister")
@@ -15,4 +21,10 @@ public class TradeRegister {
         return "tradeRegister/trade_register";
     }
 
+	@RequestMapping(value = "productRegister.do", method = RequestMethod.POST)
+	@ResponseBody
+    public Object productRegister() {
+        int is_register = data_Center_Service.getItem();
+        return list;
+    }
 }
